5d05a8e05e2e1f81ef34fa6ff0b3ba6c7c5a9df6
Ryo Shimizu <ryo@nerv.org>
2021-02-18 14:00:55 +0900

    sync rcsid

--- if_aq.c
+++ if_aq.c
@@ -62,7 +62,7 @@
 //#define XXX_DUMP_RSSKEY
 //#define XXX_ONLY_8_DESCRIPTOR_TEST
 
-/*	$NetBSD: if_aq.c,v 1.18 2020/09/24 05:09:46 ryo Exp $	*/
+/*	$NetBSD: if_aq.c,v 1.19 2020/09/24 05:13:03 ryo Exp $	*/
 
 /**
  * aQuantia Corporation Network Driver
@@ -126,7 +126,7 @@
  */
 
 #include <sys/cdefs.h>
-__KERNEL_RCSID(0, "$NetBSD: if_aq.c,v 1.18 2020/09/24 05:09:46 ryo Exp $");
+__KERNEL_RCSID(0, "$NetBSD: if_aq.c,v 1.19 2020/09/24 05:13:03 ryo Exp $");
 
 #ifdef _KERNEL_OPT
 #include "opt_if_aq.h"
